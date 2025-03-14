2eb9ab038d866935c15330345a6216d88f9d424c
Evan Brody <brody_e@outlook.com>
2025-03-14 09:52:36 -0400

    .

--- dualLPbound.py
+++ dualLPbound.py
@@ -1,51 +1,72 @@
 import numpy as np
 import itertools as it
 
-def getQ(n, d):
-    return 0
+class RP:
+    __slots__ = ('d', 'n', 'Q', 'subQs', 'S', 'a')
 
-def u(S, a, d, Q):
-    n = len(a)
-    return 1
+    def __init__(self, d, n):
+        self.d = d
+        self.n = n
+        self.initQs()
+        self.a = np.ones(self.n, dtype=int)
 
-def uS(S, a, i, d, Q):
-    Si = S.copy(); Si[i] = True
-    return u(Si, a, d, Q) - u(S, a, d, Q)
+    def initQs(self):
+        self.Qs = np.empty(self.d - 2) # (d - 2) - 2 + 1 + 1
+        self.Q = self.Qs[0] = self.d # 1-certificate
+        # TODO: this could probably be done better
+        for i in range(1, self.d - 2): # [2, d - 2]
+            # seen (i + 1) unique reps AND performed n - d + 1 + (i + 1) tests
+            thisQ = (i + 1) + (self.n - self.d + 1 + (i + 1))
+            self.Qs[i] = thisQ; self.Q *= thisQ
 
-def expression(S, a, d, Q):
-    numerator = sum(
-        [ uS(S, a, i, d, Q) for i, selected in enumerate(S) if not selected ]
-    )
-    return numerator / (Q - u(S, a, d, Q))
+    def u(self):
+        subtractor = 1
+        num_tests = len(list(filter(bool, self.S)))
+        unique = len({ e for e in it.compress(self.a, self.S) })
+        for i in range(2, self.d - 1):
+            subtractor
 
-def all_subsets(a, d, Q):
-    n = len(a)
-    S = np.zeros(n, dtype=bool)
-    max_so_far = -1
-    while True:
-        max_so_far = max(max_so_far, expression(S, a, d, Q))
-        i = 0
-        while i < n and S[i]:
-            S[i] = False; i += 1
-        if n == i:
-            return max_so_far
-        S[i] = True
+        return self.Q - subtractor
 
-def bound(n, d):
-    a = np.array([1] * n, dtype=int)
-    max_so_far = -1
-    Q = getQ(n, d)
-    while 1:
-        max_so_far = max(max_so_far, all_subsets(a, d, Q))
-        
-        i = 1
-        while i < n + 1 and d == a[-i]:
-            a[-i] = 1; i += 1
-        if n + 1 == i:
-            return max_so_far
-        a[-i] += 1
+    def uS(self, i):
+        assert not self.S[i]
 
-        print(a)
+        u = self.u()
 
-bound(3, 4)
-all_subsets([1, 2, 3], 3, 9)
+        self.S[i] = True
+        uSi = self.u()
+
+        self.S[i] = False
+        assert uSi - u > 0
+        return uSi - u
+
+    def expression(self):
+        numerator = sum(
+            [ self.uS(i) for i, _ in enumerate(it.compress(self.a, not self.S)) ]
+        )
+        return numerator / (self.Q - self.u())
+
+    def all_subsets(self):
+        self.S = np.zeros(self.n, dtype=bool)
+        max_so_far = -1
+        while True:
+            max_so_far = max(max_so_far, self.expression())
+
+            i = 0
+            while i < self.n and self.S[i]:
+                self.S[i] = False; i += 1
+            if self.n == i:
+                return max_so_far
+            self.S[i] = True
+
+    def bound(self):
+        max_so_far = -1
+        while True:
+            max_so_far = max(max_so_far, self.all_subsets())
+            
+            i = 1
+            while i < self.n + 1 and self.d == self.a[-i]:
+                self.a[-i] = 1; i += 1
+            if self.n + 1 == i:
+                return max_so_far
+            self.a[-i] += 1
