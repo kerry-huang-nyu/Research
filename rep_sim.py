72aa35c1be3e627ae46b87a35ddca7bde9d521ad
Kerry Huang <kerryhuang@mac.mynetworksettings.com>
2025-04-09 02:39:23 -0400

    this is deplorable, but at the same time this is deployable

--- rep_sim.py
+++ rep_sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools as it
+from tqdm import tqdm
 
 class RepProb:
     def __init__(self, d, n):
@@ -18,22 +19,61 @@ class RepProb:
 
         factor = ERR / Eopt
         if factor > self.d:
+            print("\n")
             print('dist: ', self.distribution)
             print('opt: ', opt)
             print('E[opt]: ', Eopt)
             print('RR: ', rr_ordering)
             print('E[RR]: ', ERR)
+            print("\n")
 
-    def expected(self, order):
+    def expected(self, order):  
         # Abandon all hope ye who enter here
-        pass
+        # each "bit" could be {0, 1, ... d-1} 
+        # why go for efficiency when you can go for a d^n algo? 
+        # stop early once we find all representatives by the order 
+
+        total_expected = 0.0
+
+        combinations = set() #all combinations of dice seen. 
+        #if [1, 0, 2, 2, 2] has been seen [1, 0, 2] will be stored to prevent [1, 0, 2, 1, 0] from being calculated 
+        # All combinations of outcomes (one per test)
+
+        for outcome in it.product(range(self.d), repeat=len(order)): #generate vector len n with values = {1 - d}
+            seen = set()
+            steps = 0
+            prob = 1.0 #compute probability until we see a 1 or 0 certificate 
+
+            for i in range(len(order)):
+                #dice is the nth dice we are pulling {0 ... n - 1}
+                #val is the value on that dice we generated  {0 ... d-1}
+                #sampling the ith value because it is easier to slide outcome that way 
+                dice = order[i]
+                val = outcome[i]
+                seen.add(val)
+                
+                steps += 1
+                chance = self.distribution[0, dice] if val == 0 else self.distribution[val, dice] - self.distribution[val - 1, dice]
+                total = self.distribution[self.d-1, dice]
+                prob *= chance / total
+
+                if len(seen) == self.d:
+                    fingerprint = tuple(outcome[:steps])
+                    if fingerprint in combinations:
+                        steps = 0 #do not add the value into the expected if we have already found the fingerprint 
+                    combinations.add(fingerprint)
+                    break
+
+            total_expected += prob * steps
+
+        return total_expected
 
     def opt_search(self):
         current_min = float('inf')
         current_best = None
 
         # Brute force through all orderings
-        for order in it.permutations(range(len(self.distribution))):
+        for order in it.permutations(range(self.n)): #wait is this supposed to be a length of 5? 
             current_exp = self.expected(order)
             if current_exp < current_min:
                 current_min = current_exp
@@ -65,7 +105,7 @@ class RepProb:
         while turn < self.n:
             for thread in threads:
                 # Don't add the same test twice
-                if not tested[thread[turn]]:
+                if thread[turn] not in tested:
                     rr_ordering.append(thread[turn])
                     tested.add(thread[turn])
             turn += 1
@@ -75,7 +115,7 @@ class RepProb:
 
 if __name__ == '__main__':
     d = 3
-    n = 5
+    n = 7
     rep_prob = RepProb(d, n)
-    for _ in range(1_000):
+    for _ in tqdm(range(1_000), desc="Running simulations"):
         rep_prob.check_opt_vs_rr()
